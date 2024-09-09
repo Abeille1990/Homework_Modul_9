881935f98b14d76ad66d55306169a29d0e30fbf6
Abeille1990 <tatiana.tyukova@gmail.com>
2024-09-09 23:50:03 +0300

    Add files via upload

--- modul_9_2.py
+++ modul_9_2.py
@@ -4,9 +4,21 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 first_result = [len(x) for x in first_strings if len(x) > 5]
 print(first_result)
 
-second_result = [f"('{x}','{y}')" for x in first_strings for y in second_strings if len(x) == len(y)]
+second_result = ['('+x+','+y+')' for x in first_strings for y in second_strings if len(x) == len (y)]
 print(second_result)
+
+#second_result = [f"('{x}','{y}')" for x in first_strings for y in second_strings if len(x) == len(y)]
+#print(second_result)
+
+#second_result = [tuple(x+y,) for x in first_strings for y in second_strings if len(x) == len (y)]
+#print(second_result)
+
 
 a = first_strings + second_strings
 third_result = {x: len(x) for x in a if len(x) % 2 == 0}
 print(third_result)
+
+# third_result1 = [{x:len(x) for x in first_strings if len(x)%2==0 }]
+# + [{y:len(y) for y in second_strings if len(y)%2==0}]
+# print(third_result1)
+
