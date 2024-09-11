926e019ad4ad273d62fc1fde3c506d4125174996
Abeille1990 <tatiana.tyukova@gmail.com>
2024-09-11 20:43:31 +0300

    Add files via upload

--- modul_9_3.py
+++ modul_9_3.py
@@ -8,3 +8,23 @@ second_result = ((abs(len(first[x]))==len(second[x])) for x in range(max(len(fir
 
 print(list(first_result))
 print(list(second_result))
+
+
+# Задача:
+# Дано 2 списка:
+# first = ['Strings', 'Student', 'Computers']
+# second = ['Строка', 'Урбан', 'Компьютер']
+# Необходимо создать 2 генераторных сборки:
+# В переменную first_result запишите генераторную сборку, которая высчитывает
+# разницу длин строк из списков first и second, если их длины не равны.
+# Для перебора строк попарно из двух списков используйте функцию zip.
+# В переменную second_result запишите генераторную сборку,
+# которая содержит результаты сравнения длин строк в одинаковых позициях из списков first и second.
+# Составьте эту сборку НЕ используя функцию zip. Используйте функции range и len.
+
+# Пример выполнения кода:
+# print(list(first_result))
+# print(list(second_result))
+# Вывод в консоль:
+# [1, 2]
+# [False, False, True]
